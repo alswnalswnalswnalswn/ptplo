95cd102e5b38d7858f5d876275298a3f26919be5
minju <chlalswn281@naver.com>
2024-04-23 20:57:27 +0900

    세팅/로그인 끝!!

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/50/e0e16cf96201001f1811b5eb490fb93b
@@ -0,0 +1,6 @@
+package com.kh.thenolja.member.model.service;
+
+public class MemberServiceImpl implements MemberService {
+	
+	
+}
--- semiProject/src/main/java/com/kh/thenolja/member/model/service/MemberService.java
+++ semiProject/src/main/java/com/kh/thenolja/member/model/service/MemberService.java
@@ -1,6 +1,16 @@
 package com.kh.thenolja.member.model.service;
 
+import com.kh.thenolja.member.model.vo.Member;
+
 public interface MemberService {
 	
+	Member login(Member member);
+	
+	int update(Member member);
+	
+	int delete(Member member);
+	
+	
+	int idCheck(String chekcId);
 
 }
